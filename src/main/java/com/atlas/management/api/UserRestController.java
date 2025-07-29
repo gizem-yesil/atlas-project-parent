9ee66d7493d1abf39ff690ec2c2d14b149d5a6d4
gizem-yesil <yesiloglu.gzm@gmail.com>
2025-07-29 14:06:51 +0300

    backend register

---
+++ src/main/java/com/atlas/management/api/UserRestController.java
@@ -0,0 +1,37 @@
+package com.atlas.management.api;
+
+import com.atlas.management.entity.User;
+import com.atlas.management.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(origins = "*") // Frontend erişimi için CORS ayarı (gerekirse sınırlandırabiliriz)
+public class UserRestController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+   @GetMapping("/")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id) {
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+    }
+}
--- src/main/java/com/atlas/management/service/UserService.java
+++ src/main/java/com/atlas/management/service/UserService.java
@@ -1,7 +1,34 @@
 package com.atlas.management.service;
 
+import com.atlas.management.entity.User;
+import com.atlas.management.repository.UserRepository;
+
 import org.springframework.stereotype.Service;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+
 }
